4c0e6a96c36a721ff8c0d12a2c9a28081895d95c
ijzm <ignacio.zevallos@hotmail.com>
2016-01-23 00:55:19 -0500

    Added some more tiles, edited maps

--- Preloader.js
+++ Preloader.js
@@ -15,11 +15,15 @@ Quack.Preloader.prototype = {
 		
 		this.load.image('bullet', 'assets/bullet.png');
 		
+		this.load.image('sheep', 'assets/sheep.png');
+		
 		this.load.spritesheet('player', 'assets/player.png', 35, 35);
 		//Music/Sounds
 		this.load.audio('menumusic', 'assets/menumusic.ogg');
 		//Tilemaps and Tiles
 		this.load.tilemap('0', 'maps/0.json', null, Phaser.Tilemap.TILED_JSON);
+		this.load.tilemap('1', 'maps/1.json', null, Phaser.Tilemap.TILED_JSON);
+		
 		this.load.image('tiles', 'assets/tiles.png');
 
 		this.time.advancedTiming = true;
