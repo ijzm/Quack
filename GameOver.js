b98ecca10ac3774e4ddd41046a07ab8c166522be
ijzm <ignacio.zevallos@hotmail.com>
2016-01-25 22:28:17 -0500

    Added Space to continue

--- GameOver.js
+++ GameOver.js
@@ -7,11 +7,17 @@ Quack.GameOver.prototype = {
 
 	create: function () {
 		this.game.add.sprite(0, 0, 'bg');
-
 		this.add.button(0, 0, "playbutton", this.playTheGame, this);
 	},
 
-	update: function () {},
+	update: function () {
+		var wasd = {
+			space: this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR),
+		}
+		if (wasd.space.isDown) {
+			this.playTheGame();
+		}
+	},
 	playTheGame: function () {
 		console.log("PLAY");
 		this.state.start("Game");
